46127f26901f3a8ed1b4220e225fa81c14802eba
Alexander Trukhin <alxtry@gmail.com>
2015-06-29 22:21:06 +0300

    Add output writing class for the Metrics abstraction.
    No functional changes.
    This helper class could be used in glretrace to output new profiling
    metrics data.

---
+++ retrace/metric_writer.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <queue>
+#include <vector>
+#include <string>
+
+#include "metric_backend.hpp"
+
+struct ProfilerCall {
+    int no;
+    unsigned program;
+    std::string name;
+    unsigned eventId;
+};
+
+struct ProfilerFrame {
+    unsigned eventId;
+};
+
+class MetricWriter
+{
+private:
+    bool perFrame;
+    static bool header;
+    std::queue<ProfilerCall> callQueue;
+    std::queue<ProfilerFrame> frameQueue;
+    std::vector<MetricBackend*>* metricApis;
+
+public:
+    MetricWriter(std::vector<MetricBackend*>* _metricApis) : perFrame(false), metricApis(_metricApis) {}
+
+    void addCall(int no,
+                 const char* name,
+                 unsigned program, unsigned eventId);
+
+    void addFrame(unsigned eventId);
+
+    static void writeApiData(Metric* metric, int event, void* data, int error, void* userData);
+
+    void writeCall();
+
+    void writeFrame();
+
+    void writeAll();
+};
