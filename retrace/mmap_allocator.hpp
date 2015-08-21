1ab675d3a5b5a3415963f738e92f6057a21506a7
Alexander Trukhin <alxtry@gmail.com>
2015-08-21 10:44:44 +0300

    Add custom allocator that uses mmap. Also make metric_writer use it.
    Profiling uses a lot of memory as all data is kept through the whole
    run. This is an attempt to reduce the usage.
    MmapAllocator creates temporary files on disk and mmaps them.
    It can be used by STL containers.
    Currently enabled only on unix-like systems.
    The idea is to use this allocator in writer and backends where otherwise
    data significantly increases RAM usage.

---
+++ retrace/mmap_allocator.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <iostream>
+#include <memory>
+#include <list>
+
+#ifdef __unix__
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+class MmapedFileBuffer
+{
+private:
+    int fd;
+    off_t curChunkSize;
+    const size_t chunkSize;
+    std::list<void*> mmaps;
+    void* vptr;
+
+    MmapedFileBuffer(MmapedFileBuffer const&) = delete;
+
+    void operator=(MmapedFileBuffer const&) = delete;
+
+    void newMmap() {
+        ftruncate(fd, chunkSize * (mmaps.size() + 1));
+        vptr = mmap(NULL, chunkSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
+                    chunkSize * mmaps.size());
+        mmaps.push_front(vptr);
+        curChunkSize = 0;
+    }
+
+public:
+    MmapedFileBuffer()
+        : curChunkSize(0),
+          chunkSize((128 * 1024 * 1024L) & ~(sysconf(_SC_PAGE_SIZE) - 1))
+    {
+        char templ[] = ".pbtmpXXXXXX";
+        fd = mkstemp(templ);
+        newMmap();
+    }
+
+    ~MmapedFileBuffer() {
+        close(fd);
+        for (auto &m : mmaps) {
+            munmap(m, chunkSize);
+        }
+    }
+
+    void* allocate(size_t size) {
+        if ((curChunkSize + size) > chunkSize) {
+            newMmap();
+        }
+        void* addr = static_cast<char*>(vptr) + curChunkSize;
+        curChunkSize += size;
+        return addr;
+    }
+};
+
+template <class T>
+class MmapAllocator
+{
+private:
+    std::shared_ptr<MmapedFileBuffer> file;
+    void* vptr;
+
+    template<class U>
+    friend class MmapAllocator;
+
+public:
+    typedef T value_type;
+
+    MmapAllocator()
+    {
+        file = std::make_shared<MmapedFileBuffer>();
+    };
+
+    ~MmapAllocator() {
+    };
+
+    template <class U> MmapAllocator(const MmapAllocator<U>& other)
+        : file(other.file), vptr(other.vptr) {
+    };
+
+    T* allocate(std::size_t n) {
+        return reinterpret_cast<T*>(file->allocate(n * sizeof(T)));
+    };
+
+    void deallocate(T* p, std::size_t n) {};
+};
+template <class T, class U>
+bool operator==(const MmapAllocator<T>&, const MmapAllocator<U>&) {
+    return true;
+}
+template <class T, class U>
+bool operator!=(const MmapAllocator<T>& a, const MmapAllocator<U>& b) {
+    return !(a==b);
+}
+
+#else
+// default allocator
+template<class T>
+using MmapAllocator = std::allocator<T>
+
+#endif
